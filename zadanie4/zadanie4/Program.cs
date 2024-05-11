2fb9877f02972341bf69a7e79b627961b5c786fe
s23551 <s23551@pjwstk.edu.pl>
2024-05-11 17:57:16 +0200

    Fill Program.cs configuration.

--- zadanie4/zadanie4/Program.cs
+++ zadanie4/zadanie4/Program.cs
@@ -1,9 +1,14 @@
+using zadanie4.Model;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+builder.Services.AddScoped<IWarehouseRepository, WarehouseRepository>();
+builder.Services.AddScoped<IWarehouseService, WarehouseService>();
 
 var app = builder.Build();
 
@@ -15,6 +20,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.MapControllers();
 
 app.Run();
 
